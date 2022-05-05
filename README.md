6f7389e5c4eabe01aee94629ffe68148cbaa013a
thomshouse@gmail.com <thomshouse@gmail.com>
2022-05-04 17:25:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# thomshouse-ellipsis/vscode template
+
+Dotfiles package for Visual Studio Code by [thomshouse][thomshouse].
+
+## Install
+
+Clone and symlink or install with [ellipsis][ellipsis]:
+
+```
+$ ellipsis install thomshouse-ellipsis/vscode
+```
+
+[ellipsis]: http://ellipsis.sh
+[thomshouse]: https://github.com/thomshouse
---
+++ ellipsis.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+# List of vscode extensions to install
+extensions=(
+);
+
+pkg.install() {
+    case $(os.platform) in
+        osx)
+            ## Install homebrew cask(s)
+            if utils.cmd_exists brew; then
+                brew install --cask visual-studio-code
+                brew install jq
+            fi
+            VSCONFIGPATH="$HOME/Library/Application\ Support/Code/User"
+            ;;
+        wsl2)
+            ## Install dot-chocolatey if it's not installed
+            if ! utils.cmd_exists choco; then
+                $ELLIPSIS_PATH/bin/ellipsis installed | grep 'chocolatey' >/dev/null 2>&1
+                if [ $? -eq 0 ]; then
+                    $ELLIPSIS_PATH/bin/ellipsis pull chocolatey
+                    $ELLIPSIS_PATH/bin/ellipsis reinstall chocolatey
+                else
+                    $ELLIPSIS_PATH/bin/ellipsis install thomshouse-ellipsis/chocolatey
+                fi
+            fi
+            ## Install choco package(s)
+            if utils.cmd_exists choco; then
+                choco install vscode -y
+            fi
+            ## Install apt-package(s)
+            if utils.cmd_exists apt-get; then
+                DEBIAN_FRONTEND=noninteractive sudo apt-get update -y && sudo apt-get install -y jq
+            fi
+            VSCONFIGPATH="$(wslpath -u "$(cmd.exe /C echo %APPDATA% 2> /dev/null | sed -e 's/\r//g')")/Code/User"
+            ;;
+        linux)
+            ## Install apt-package(s)
+            if utils.cmd_exists apt-get; then
+                wget -O "${TMPDIR:-/tmp}/vscode.deb" https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
+                DEBIAN_FRONTEND=noninteractive sudo apt-get update -y && sudo apt install -y "${TMPDIR:-/tmp}/vscode.deb"
+                DEBIAN_FRONTEND=noninteractive sudo apt-get install -y jq
+            fi
+            VSCONFIGPATH="$HOME/.config/Code/User"
+            ;;
+    esac
+
+    # Merge the config files
+    jq -s '.[0] * .[1] * .[2]' "$PKG_PATH/config/defaults.json" "$VSCONFIGPATH/settings.json" "$PKG_PATH/config/overrides.json" > "${TMPDIR:-/tmp}/vscode-settings.json"
+    mv "${TMPDIR:-/tmp}/vscode-settings.json" "$VSCONFIGPATH/settings.json"
+
+    # Install each vscode extension
+    for extension in "${extensions[*]}"; do
+        code --install-extension "$extension"
+    done
+
+    # Initialize the package
+    pkg.init
+}
+
+pkg.init() {
+    # Add package bin to $PATH
+    if [ -d "$PKG_PATH/bin" ]; then
+        export PATH=$PKG_PATH/bin:$PATH
+    fi
+
+    # Run init scripts
+    for file in $(find "$PKG_PATH/init" -maxdepth 1 -type f -name "*.zsh" 2>/dev/null); do
+        [ -e "$file" ] || continue
+        . "$file"
+    done
+}
+
+pkg.link() {
+    # fs.link_files links;
+    : # Package does not contain linkable files
+}
+
+pkg.unlink() {
+    : # Package does not contain linkable files
+}
